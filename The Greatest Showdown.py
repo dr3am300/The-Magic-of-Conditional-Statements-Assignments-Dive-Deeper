ac0a8cda8308e9c112d9a6155182e183e4a1eabd
dr3am300 <dream300it@gmail.com>
2024-06-03 19:56:06 -0400

    “FINISHED”

--- The Greatest Showdown.py
+++ The Greatest Showdown.py
@@ -3,12 +3,12 @@
 # Task 1: Identify the Greatest------------------------------------------
 # Write a Python program that prompts the user to enter three numbers. The program should then identify and print out the largest number among the three.
 
-number8 = int(input("Enter the first number: "))
+number1 = int(input("Enter the first number: "))
 number2 = int(input("Enter the second number: "))
 number3 = int(input("Enter the third number: "))
-if number8 > number2 and number8 > number3:
-    print("The greatest number is", number8)
-elif number2 > number8 and number2 > number3:
+if number1 > number2 and number1 > number3:
+    print("The greatest number is", number1)
+elif number2 > number1 and number2 > number3:
     print("The greatest number is", number2)
 else:
     print("The greatest number is", number3)
@@ -16,12 +16,12 @@ else:
 # Task 2: Identify the Smallest---------------------------------------------
 #Extend your program from Task 1 to also determine the smallest number among the three and print it out.
 
-number7 = int(input("Enter the first number: "))    
+number1 = int(input("Enter the first number: "))    
 number2 = int(input("Enter the second number: "))
 number3 = int(input("Enter the third number: "))
-if number7 < number2 and number7 < number3:
-    print("The smallest number is", number7)
-elif number2 < number7 and number2 < number3:
+if number1 < number2 and number1 < number3:
+    print("The smallest number is", number1)
+elif number2 < number1 and number2 < number3:
     print("The smallest number is", number2)
 else:
     print("The smallest number is", number3)
@@ -33,14 +33,18 @@ else:
 # The largest number is 4. There are two numbers equal to each other." 
 # Printing out which numbers are equal would be a great added bonus.
 
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+number3 = int(input("Enter the third number: "))
+
+if number1 == number2 == number3 or number1 == number2 or number1 == number3 or number2 == number3:
+    print("All numbers are equal.")
+elif number1 > number2 or number1 > number3:
+    print("The greatest number is", number1)
+elif number2 > number1 or number2 > number3:
+    print("The greatest number is", number2)
+else:
+    print("The greatest number is", number3)
 
 
-    
-     
-   
-
-
-        
-        
-        
 
