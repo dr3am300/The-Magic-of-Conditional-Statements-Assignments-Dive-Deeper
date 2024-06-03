2e4627d15b77639345a8ea99433e6c7ae4cb254e
dr3am300 <dream300it@gmail.com>
2024-06-03 10:52:08 -0400

    adding a print statement

--- Decisions at the Crossroad.py
+++ Decisions at the Crossroad.py
@@ -2,7 +2,7 @@
 # You are provided with a Python script that uses conditional statements to tell if a number is positive, negative, or zero, but it has some errors. 
 # Identify and fix them.
 #Buggy Code:
-
+print ("welcome to the decisions at the crossroad")
 number = int(input("Enter a number: "))
 if number > 0:
     print("The number is positive.")
